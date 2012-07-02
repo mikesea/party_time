889d6d668c22210c5319197fc2bb334f72b9010a
Mike Chlipala <mchlipala@gmail.com>
2012-07-02 19:38:32 -0400

    updated config stuff

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,7 +16,6 @@ Dir[Rails.root.join("spec/factories/*.rb")].each {|f| require f}
 
 require 'ripple/test_server'
 
-
 RSpec.configure do |config|
 
   config.before(:suite) { Ripple::TestServer.setup }
@@ -31,12 +30,12 @@ RSpec.configure do |config|
   # config.mock_with :rr
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  #  config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  # config.use_transactional_fixtures = true
 
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
