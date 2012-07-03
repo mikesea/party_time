fa2bd314adec137b4a8fcc0f4acde5f61e3a12d3
Mike Chlipala <mchlipala@gmail.com>
2012-07-02 21:34:49 -0400

    new styling

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,7 @@ Dir[Rails.root.join("spec/factories/*.rb")].each {|f| require f}
 
 require 'ripple/test_server'
 
+
 RSpec.configure do |config|
 
   config.before(:suite) { Ripple::TestServer.setup }
