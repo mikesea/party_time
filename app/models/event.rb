a4bf8a654f4518573ef062f9d9c14925e5856a5b
Mike Chlipala <mchlipala@gmail.com>
2012-07-03 07:38:33 -0400

    removed event models

--- app/models/event.rb
+++
@@ -1,8 +0,0 @@
-class Event
-  include Ripple::EmbeddedDocument
-  property :date, Time
-  property :name, String
-  property :details, String
-  property :location, String
-  embedded_in :playlist
-end
